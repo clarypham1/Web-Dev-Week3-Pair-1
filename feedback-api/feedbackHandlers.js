c310dd87ef2b120b55a1f033c82e9a20ed35ba7f
Tara <mtaraalleah@gmail.com>
2026-08-31 15:20:14 +0300

    feat(api): add feedback request handlers

--- feedback-api/feedbackHandlers.js
+++ feedback-api/feedbackHandlers.js
@@ -1,19 +1,21 @@
+const Feedback = required("./feedbackLib");
+
 const getAllFeedbacks = (req, res) => {
-    res.json({message: "tara and clary are awesome"});
+    res.json({message: "Hello from get all feedbacks"});
 };
 
 const createFeedback = (req, res) => {
-    res.json({message: "tara and clary"});
+    res.json({message: "Hello from create feedbacks"});
 };
 
 const getFeedbackById = (req, res) => {
-    res.json({message: "tara and clary are awesome"});
+    res.json({message: "Hello from get feedbacks by Id"});
 };
 const updateFeedback = (req, res) => {
-    res.json({message: "tara and clary are awesome"});
+    res.json({message: "Hello from update feedbacks"});
 };
 const deleteFeedback = (req, res) => {
-    res.json({message: "tara and clary are awesome"});
+    res.json({message: "Hello from delete feedbacks"});
 };
 
 module.exports = {
