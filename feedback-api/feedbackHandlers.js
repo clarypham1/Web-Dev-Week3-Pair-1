94eacbf6a71ba564c2082639c98ab40140c8bd28
Clary <claryphp@metropolia.fi>
2026-08-31 15:47:03 +0300

    feat(api): create feedback

--- feedback-api/feedbackHandlers.js
+++ feedback-api/feedbackHandlers.js
@@ -6,17 +6,29 @@ const getAllFeedbacks = (req, res) => {
 };
 
 const createFeedback = (req, res) => {
-    res.json({message: "Hello from create feedbacks"});
+    const { sender, message, rating, platform } = req.body;
+    const newFeedback = Feedback.addOne(
+        sender,
+        message,
+        rating,
+        platform
+    );
+    
+    if (newFeedback) {
+        res.status(201).json(newFeedback);
+    } else {
+        res.status(500).json({ message: "Failed to create feedback" });
+    }
 };
 
 const getFeedbackById = (req, res) => {
-    res.json({message: "Hello from get feedbacks by Id"});
+    res.json({ message: "Hello from get feedbacks by Id" });
 };
 const updateFeedback = (req, res) => {
-    res.json({message: "Hello from update feedbacks"});
+    res.json({ message: "Hello from update feedbacks" });
 };
 const deleteFeedback = (req, res) => {
-    res.json({message: "Hello from delete feedbacks"});
+    res.json({ message: "Hello from delete feedbacks" });
 };
 
 module.exports = {
